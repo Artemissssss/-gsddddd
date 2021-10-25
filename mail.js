da8b7ad9855f109a1b271f41d87e7f704aed9e7f
Artemissssss <64923345+Artemissssss@users.noreply.github.com>
2021-10-25 14:29:23 +0300

    Add files via upload

---
+++ mail.js
@@ -0,0 +1,48 @@
+const NAMEISREQUIRED = true;
+const MSGSNAMEERROR = "Поле обязательно для заполнения";
+
+// Телефон
+const TELISREQUIRED = false;
+const MSGSTELERROR = "Поле обязательно для заполнения";
+
+// Email
+const EMAILISREQUIRED = false;
+const MSGSEMAILERROR = "Поле обязательно для заполнения";
+const MSGSEMAILINCORRECT = "Некорректный почтовый адрес";
+
+// Текстовое поле
+const TEXTISREQUIRED = false;
+const MSGSTEXTERROR = "Поле обязательно для заполнения";
+
+// Файл
+const FILEISREQUIRED = false;
+const MSGSFILEERROR = "Поле обязательно для заполнения";
+
+// Соглашение
+const AGGREMENTISREQUIRED = false;
+const MSGSAGGREMENTERROR = "Поле обязательно для заполнения"; 
+
+// Сообщение об успешной отправке
+const MSGSSUCCESS = "Сообщение успешно отправлено";
+
+// *** SMTP *** //
+
+  require_once($_SERVER['DOCUMENT_ROOT'] . '/index.php');
+  const HOST = 'ssl://smtp.yandex.ru';
+  const LOGIN = 'sender@yandex.ru';
+  const PASS = 'senderPass';
+  const PORT = '465';
+
+// *** /SMTP *** //
+
+      // Почта с которой будет приходить письмо
+const SENDER = 'astrakiller20@gmail.com';
+ 
+// Почта на которую будет приходить письмо
+const CATCHER = 'artemka7.av@gmail.com';
+ 
+// Тема письма
+const SUBJECT = 'Заявка с сайта';
+ 
+// Кодировка
+const CHARSET = 'UTF-8';
